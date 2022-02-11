dac95af0bfa34d8bbe8118bdb4a7807a9f3c25af
mackycheese21 <jackarmstrong@berkeley.edu>
2022-02-10 23:03:07 -0800

    quickfix strings

--- src/main/java/mackycheese21/ferricoxide/nast/ll/expr/LLStringConstant.java
+++ src/main/java/mackycheese21/ferricoxide/nast/ll/expr/LLStringConstant.java
@@ -19,8 +19,8 @@ public class LLStringConstant extends LLExpression {
     @Override
     public void compile(LLContext ctx) {
         value = new LLValue(LLType.pointer(LLType.u8()), LLVM.LLVMBuildInBoundsGEP2(ctx.builder(),
-                LLVM.LLVMArrayType(LLVM.LLVMIntType(8), constant.length()),
-                LLVM.LLVMConstString(constant, constant.length(), 0),
+                LLVM.LLVMArrayType(LLVM.LLVMIntType(8), constant.length() + 1),
+                LLVM.LLVMBuildGlobalString(ctx.builder(), constant, "str"),
                 new PointerPointer<>(2).put(new LLVMValueRef[]{
                         LLVM.LLVMConstInt(LLVM.LLVMIntType(32), 0, 0),
                         LLVM.LLVMConstInt(LLVM.LLVMIntType(32), 0, 0)
